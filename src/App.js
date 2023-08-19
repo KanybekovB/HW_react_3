c8adc12ade01788e0aaf699a9e79b832f3f90a9f
Bektur Kanybekov <kanybekovbektur2007@gmail.com>
2023-08-19 09:53:49 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function CreateToDo(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="inputButton">
+      <input
+        className="todoInput"
+        id="todo"
+        value={props.value} 
+        placeholder="To-Do..."
+        onChange={props.onChange}
+      ></input>
+      <button className="todoButton" onClick={props.onClick}>
+        Create
+      </button>
     </div>
   );
 }
 
-export default App;
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: null,
+      todoList: [],
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleClick = () => {
+    const newTodoList = [...this.state.todoList, this.state.inputValue];
+    this.setState({
+      todoList: newTodoList,
+      inputValue: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-container">
+        <CreateToDo
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+          onClick={this.handleClick}/>
+        <div className="todoList">
+          <div className="Tasks">Tasks</div>
+          {this.state.todoList.map((task, index) =>(
+          <div className="task" key={index}>{index + 1}: {task}</div>
+          ))}
+        </div>
+      </div>    
+    );
+  }
+}
+export default Todo;
